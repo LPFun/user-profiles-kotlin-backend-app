493e9c965f960a5200fb78d7186a67bdcadc4b7e
Alexandr Popovich <a.popovich@alfaleads.net>
2020-09-14 13:55:06 +0300

    delete abstract KmpProfile

--- profile-transport-kmp-models/src/commonMain/kotlin/com/lpfun/transport/multiplatform/profile/KmpProfile.kt
+++
@@ -1,9 +0,0 @@
-package com.lpfun.transport.multiplatform.profile
-
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
-
-@Serializable
-abstract class KmpProfile(
-        @Transient open var id: String? = null
-)
--- profile-transport-kmp-models/src/commonMain/kotlin/com/lpfun/transport/multiplatform/profile/education/model/KmpProfileEducation.kt
+++ profile-transport-kmp-models/src/commonMain/kotlin/com/lpfun/transport/multiplatform/profile/education/model/KmpProfileEducation.kt
@@ -1,14 +1,11 @@
 package com.lpfun.transport.multiplatform.profile.education.model
 
-import com.lpfun.transport.multiplatform.profile.KmpProfile
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class KmpProfileEducation(
-        override var id: String? = null,
+        var id: String? = null,
         var mainEducation: MutableList<KmpEducationModel>? = null,
         var additionalEducation: MutableList<KmpAdditionalEducationModel>? = null
-) : KmpProfile(
-        id = id
 )
 
