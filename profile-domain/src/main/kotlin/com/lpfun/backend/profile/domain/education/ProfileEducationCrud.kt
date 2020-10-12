ba86b06caa32f752a4daffe43bd501f04d66bee5
Alexandr Popovich <a.popovich@alfaleads.net>
2020-10-12 15:51:30 +0300

    rename chain to usecase

--- profile-domain/src/main/kotlin/com/lpfun/backend/profile/domain/education/ProfileEducationCrud.kt
+++ profile-domain/src/main/kotlin/com/lpfun/backend/profile/domain/education/ProfileEducationCrud.kt
@@ -3,13 +3,13 @@ package com.lpfun.backend.profile.domain.education
 import com.lpfun.backend.common.model.profile.education.ProfileEducationContext
 
 class ProfileEducationCrud {
-    private val getChain = ProfileEducationGetUseCase()
-    private val createChain = ProfileEducationCreateUseCase()
-    private val updateChain = ProfileEducationUpdateUseCase()
-    private val deleteChain = ProfileEducationDeleteUseCase()
+    private val getUseCase = ProfileEducationGetUseCase()
+    private val createUseCase = ProfileEducationCreateUseCase()
+    private val updateUseCase = ProfileEducationUpdateUseCase()
+    private val deleteUseCase = ProfileEducationDeleteUseCase()
 
-    suspend fun get(context: ProfileEducationContext) = getChain.execute(context)
-    suspend fun create(context: ProfileEducationContext) = createChain.execute(context)
-    suspend fun update(context: ProfileEducationContext) = updateChain.execute(context)
-    suspend fun delete(context: ProfileEducationContext) = deleteChain.execute(context)
+    suspend fun get(context: ProfileEducationContext) = getUseCase.execute(context)
+    suspend fun create(context: ProfileEducationContext) = createUseCase.execute(context)
+    suspend fun update(context: ProfileEducationContext) = updateUseCase.execute(context)
+    suspend fun delete(context: ProfileEducationContext) = deleteUseCase.execute(context)
 }
--- profile-domain/src/main/kotlin/com/lpfun/backend/profile/domain/personal/ProfilePersonalCrud.kt
+++ profile-domain/src/main/kotlin/com/lpfun/backend/profile/domain/personal/ProfilePersonalCrud.kt
@@ -3,13 +3,13 @@ package com.lpfun.backend.profile.domain.personal
 import com.lpfun.backend.common.model.profile.personal.ProfilePersonalContext
 
 class ProfilePersonalCrud {
-    private val getChain = ProfilePersonalGetUseCase()
-    private val createChain = ProfilePersonalCreateUseCase()
-    private val updateChain = ProfilePersonalUpdateUseCase()
-    private val deleteChain = ProfilePersonalDeleteUseCase()
+    private val getUseCase = ProfilePersonalGetUseCase()
+    private val createUseCase = ProfilePersonalCreateUseCase()
+    private val updateUseCase = ProfilePersonalUpdateUseCase()
+    private val deleteUseCase = ProfilePersonalDeleteUseCase()
 
-    suspend fun get(context: ProfilePersonalContext) = getChain.execute(context)
-    suspend fun create(context: ProfilePersonalContext) = createChain.execute(context)
-    suspend fun update(context: ProfilePersonalContext) = updateChain.execute(context)
-    suspend fun delete(context: ProfilePersonalContext) = deleteChain.execute(context)
+    suspend fun get(context: ProfilePersonalContext) = getUseCase.execute(context)
+    suspend fun create(context: ProfilePersonalContext) = createUseCase.execute(context)
+    suspend fun update(context: ProfilePersonalContext) = updateUseCase.execute(context)
+    suspend fun delete(context: ProfilePersonalContext) = deleteUseCase.execute(context)
 }
